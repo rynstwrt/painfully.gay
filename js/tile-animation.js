9007e13b746d66e44ef308d940b56ff54d355c6d
Ryn. <rynstwrt17@gmail.com>
2025-08-18 13:18:47 -0500

    Add new favicon/start adding text

--- js/tile-animation.js
+++ js/tile-animation.js
@@ -11,34 +11,25 @@ let colorIndex = 0;
 
 
 
-const main = document.querySelector("main");
+const tileContainer = document.querySelector("#tile-container");
+
 const numTiles = GRID_DIMENSIONS.reduce((prev, curr) => prev * curr);
 for (let i = 0; i < numTiles; ++i) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
     tile.style.backgroundColor = PRIDE_COLORS[colorIndex];
-    main.appendChild(tile);
+    tileContainer.append(tile);
 }
 
 
 
-// let reversed = true;
-const tiles = utils.$(".tile");
 function createWave() {
-    animate(tiles, {
-
+    animate(".tile", {
         scale: [1, 0, 1],
         backgroundColor: [
             { to: PRIDE_COLORS[++colorIndex % PRIDE_COLORS.length] }
         ],
         rotate: [0, ".25turn"],
-        // rotate: stagger("1trn", {
-        //     grid: GRID_DIMENSIONS,
-        //     // axis: "x",
-        //     from: "center",9
-        //     reversed: true
-        //     // reversed: reversed = !reversed && !reversed
-        // }),
         delay: stagger(110, {
             grid: GRID_DIMENSIONS,
             from: "center",
